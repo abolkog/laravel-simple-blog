805fefd7512e361b187ddc0a862810ed0624aee6
Khalid Elshafie <abolkog@gmail.com>
2018-04-15 22:09:08 +0900

    Update Show Post Layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,74 +2,90 @@
 
 @section('content')
     <h1>{{ $post->title }} </h1>
-  
-    @if(!Auth::guest() && (Auth::user()->id  == $post->user_id) )
-    <div class="clearfix">
-        <a href="/posts/{{ $post->id }}/edit" class="btn btn-default">
-            <i class='fas fa-edit'></i> Edit Post
-        </a>
 
-        <div class="pull-right">
-            {!! Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=>'POST' ]) !!}
-                {{ Form::hidden('_method', 'DELETE') }}
-                <button class="btn btn-danger" type="submit">
-                    <i class="fas fa-trash"></i> Delete Post
-                </button>
-
-            {!! Form::close() !!}
-        </div>
-    </div>
-    @endif
-
-    <hr />
-    <div>
-        <img src="{{ asset('images/posts/'.$post->photo) }}" class="img-responsive" />
-        {!! $post->body !!}
-    </div>
-
-    <hr />
-
-    <h4>Comments: {{ $post->comments->count() }}</h4>
-
-    <!-- Comments List -->
-    <ul class="comments">
-
-        @foreach($post->comments as $comment)
-        <li class="comment">
-            <div class="clearfix">
-                <h4 class="pull-left">{{ $comment->user->name }}</h4>
-                <p class="pull-right">{{ $comment->created_at->format('d M Y') }}</p>
+    <div class="row">
+        <div class="col-md-8 posts">
+            <div style="margin-bottom: 20px">
+                <img src="{{ asset('images/posts/'.$post->photo) }}" class="img-responsive" />
             </div>
+            {!! $post->body !!}
 
-            <p>{{ $comment->body }}</p>
-        </li>
-        @endforeach
-    </ul>
-    <!-- Comments List -->
+            <hr />
 
+            <h4>Comments: {{ $post->comments->count() }}</h4>
 
-    <!-- Comments Form -->
-    <div class="panel panel-default">
-        <div class="panel-heading">Add Your Comment</div>
-        <div class="panel-body">
+            <!-- Comments List -->
+            <ul class="comments">
 
-            @guest
-                <div class="alert alert-info">Please login to comment</div>
-            @else
-            <form action="{{ route('comments.store', $post->slug) }}" method="POST">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="Comment">Comment</label>
-                    <textarea name="body" class="form-control" placeholder="Enter your comment" cols="30" rows="10"></textarea>
+                @foreach($post->comments as $comment)
+                <li class="comment">
+                    <div class="clearfix">
+                        <h4 class="pull-left">{{ $comment->user->name }}</h4>
+                        <p class="pull-right">{{ $comment->created_at->format('d M Y') }}</p>
+                    </div>
+
+                    <p>{{ $comment->body }}</p>
+                </li>
+                @endforeach
+            </ul>
+            <!-- Comments List -->
+            
+            <!-- Comments Form -->
+            <div class="panel panel-default">
+                <div class="panel-heading">Add Your Comment</div>
+                <div class="panel-body">
+
+                    @guest
+                        <div class="alert alert-info">Please login to comment</div>
+                    @else
+                    <form action="{{ route('comments.store', $post->slug) }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="Comment">Comment</label>
+                            <textarea name="body" class="form-control" placeholder="Enter your comment" cols="30" rows="10"></textarea>
+                        </div>
+
+                        <div class="form-group text-right">
+                            <button type="submit" class="btn btn-primary">Add Comment</button>
+                        </div>
+                    </form>
+                    @endguest
+
                 </div>
+            </div>
+            <!-- Comments Form -->
+            
+        </div>
 
-                <div class="form-group text-right">
-                    <button type="submit" class="btn btn-primary">Add Comment</button>
+        <div class="col-md-4">
+            
+            @if(!Auth::guest() && (Auth::user()->id  == $post->user_id) )
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><i class="fa fa-cog"></i> Manage</h3>
                 </div>
-            </form>
-            @endguest
+                <div class="panel-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <a href="{{ route('posts.edit', $post->id) }}">
+                                <i class="fa fa-edit"></i>Edit Post
+                            </a>
+                        </li>
+                        <li class="list-group-item">
+                             {!! Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=>'POST', 'onsubmit' => "return confirm('Are you sure you want to delete this post?')"]) !!}
+                                {{ Form::hidden('_method', 'DELETE') }}
+                                <button type="submit">
+                                    <i class="fas fa-trash"></i> Delete Post
+                                </button>
+                            {!! Form::close() !!}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            @endif
 
         </div>
-    </div>
-    <!-- Comments Form -->
+    </div><!-- Row -->
+
+    
 @endsection
