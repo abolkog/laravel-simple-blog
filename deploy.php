6ac115c5a803de8d0a155d4557ba482c4f988844
Khalid Elshafie <abolkog@gmail.com>
2018-09-07 23:56:10 +0900

    Adding Deployer

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'lblog');
+
+// Project repository
+set('repository', 'git@github.com:abolkog/laravel-simple-blog.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('149.28.132.25')
+    ->set('deploy_path', '~/{{application}}');    
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+#before('deploy:symlink', 'artisan:migrate');
+
