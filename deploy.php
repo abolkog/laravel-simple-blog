2f967717d361678ee6da489d6fa4aeb23463d99c
Khalid Elshafie <abolkog@gmail.com>
2018-09-08 00:20:30 +0900

    Update Deployment Script

--- deploy.php
+++ deploy.php
@@ -36,5 +36,5 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-#before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
 
