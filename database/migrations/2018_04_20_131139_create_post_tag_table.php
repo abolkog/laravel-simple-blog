1a06d188bfb2f687bcf822b1f488d7e73fa4df85
ramadanasr <37951976+ramadanasr@users.noreply.github.com>
2019-03-20 20:01:32 +0200

    Modify Foreign key constraints
    To fix error when trying to delete post or tag

--- database/migrations/2018_04_20_131139_create_post_tag_table.php
+++ database/migrations/2018_04_20_131139_create_post_tag_table.php
@@ -17,8 +17,8 @@ class CreatePostTagTable extends Migration
             $table->integer('post_id')->unsigned();
             $table->integer('tag_id')->unsigned();
 
-            $table->foreign('post_id')->references('id')->on('posts');
-            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             
             $table->timestamps();
         });
